0618e1b93a5f3fb3d5aa22406db62b7f5d438e60
Lucas Alves <lukasalves271@gmail.com>
2023-12-01 12:31:54 -0300

    feat: add theme base

---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#F3F2F8',
+
+    PRIMARY: '#f62252',
+    SECONDARY: '#AEDFF7',
+
+    TEXT: '#383B43',
+    TEXT_ERROR: '#ef4444',
+    SUBTEXT: '#8D919E',
+
+    WHITE: '#FFFFFF',
+    BORDER: '#C5CADA',
+  },
+
+  FONTS: {
+    TITLE: 'Inter_700Bold',
+    TEXT: 'Inter_400Regular',
+  },
+};
